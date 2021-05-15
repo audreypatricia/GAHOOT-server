4b65ed7843f22345157cbe9d9532e26db912f3fa
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-15 17:26:58 +1000

    try fix cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'http://localhost:3000', 'http://localhost:3001', 'https://gahoot-client.herokuapp.com/*' ##this needs an explicit list of your URLs on heroku
+      origins 'http://localhost:3000', 'http://localhost:3001', 'https://gahoot-client.herokuapp.com' ##this needs an explicit list of your URLs on heroku
 
       resource '*',
         headers: :any,
