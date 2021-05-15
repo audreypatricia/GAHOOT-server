335370e3ca7d9b4ddf7295c9f6575b1ab650d541
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-15 21:55:01 +1000

    added and modified appcontronller and usercontroller and sessioncontroller, added email to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :guesses
   has_and_belongs_to_many :games
   has_many :quizzes
+  has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 3 }
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
