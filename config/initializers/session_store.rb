5e16655970e6c84422647d449c68ad9c1a395697
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-17 11:57:33 +1000

    updated session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: 'gahoot', domain: 'gahoot-client.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: 'gahoot', domain: 'gahoot-client.herokuapp.com', secure:true
 else
   Rails.application.config.session_store :cookie_store, key: 'gahoot'
 end
