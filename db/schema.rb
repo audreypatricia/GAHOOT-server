6b2c32f5d585807426cf5a10f7513a7bf1ac9ba4
mac rent <macrent@macs-MacBook-Air-2.local>
2021-05-17 21:59:13 +1000

    Create Game controlls and views added to server

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_112705) do
+ActiveRecord::Schema.define(version: 2021_05_17_023151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2021_05_15_112705) do
     t.datetime "updated_at", null: false
     t.text "password_digest"
     t.text "email"
+    t.text "pin"
+    t.integer "score"
   end
 
 end
