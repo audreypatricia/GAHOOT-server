6284774dba0e5b374759f92fa5abbfc6e91c9711
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-17 11:13:37 +1000

    question controller changes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,5 +14,31 @@ class QuestionsController < ApplicationController
   def edit
   end
 
+  def create
+    image = Cloudinary: :Uploader.upload(params[:image])
+    question = Question.create :image => image["url"], :question => params[:question], :answer_options => params[:answer_options]
+    render json: question
+    # @question = Question.new(question_params)
+    #
+    # respond_to do |format|
+    #   if @question.save
+    #     format.html { redirect_to @question, notice: "Question was successfully created." }
+    #     format.json { render :show, status: :created, location: @question }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @question.errors, status: :unprocessable_entity }
+    #   end
+  end
+
+  private
+
+  def set_question
+      @question = Question.find(params[:id])
+    end
+
+  def question_params
+    params.require(:question).permit(:question, :answer_options, :image)
+  end
+
 
 end
