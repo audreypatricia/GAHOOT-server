c918038d187faa48ece1b46f476364736a74d9e8
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-20 12:26:35 +1000

    user controller update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -81,7 +81,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      @user.update :score => params[:score]
+
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :host, :pin, :score)
